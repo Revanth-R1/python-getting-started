705ba79b76e9eb38b85d2b9a9b24fb960d6891f7
Revanth Ravadagundi <67269422+Revanth-R1@users.noreply.github.com>
2022-05-05 12:58:04 +0530

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 # imported views
-import hello.views
+from hello import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
